8864d6dc27d4533638c4300c62801e5516db6f47
Josefa <289085587@qq.com>
2017-03-09 16:40:58 +0800

    front-end rebuiding

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,4 +1,60 @@
+<link rel="stylesheet" href="{{ URL::asset('css/home_index.css') }}">
 <html>
+    <body>
 
-    <p>hello laravel</p>
+    {{--header begin--}}
+    <header>
+        <section class="navigation">
+            <section class="logo-zone">
+                <div class="logo">
+                    <ul>
+                        <li>首页</li>
+                        <li>笑神本部</li>
+                        <li>论坛</li>
+                    </ul>
+                </div>
+                <div class="user-widget">
+                    <ul>
+                        <li>用户名</li>
+                        <li>短信</li>
+                        <li>放大镜</li>
+                    </ul>
+                </div>
+            </section>
+        </section>
+    </header>
+    {{--header end--}}
+
+    {{--banner begin--}}
+    <section class="banner"></section>
+    {{--banner end--}}
+
+    {{--topic begin--}}
+    <section class="topic">
+        <div class="topic-left">
+            
+        </div>
+        <div class="topic-right"></div>
+    </section>
+    {{--topic end--}}
+
+    {{--main begin--}}
+    <section class="main">
+        {{--模块一--}}
+        <section class="main-one"></section>
+        {{--模块一--}}
+
+        {{--模块二--}}
+        <section class="main-two"></section>
+        {{--模块二--}}
+    </section>
+    {{--main end--}}
+
+    {{--footer begin--}}
+    <footer>
+        <div></div>
+        <div></div>
+    </footer>
+    {{--footer end--}}
+    </body>
 </html>
