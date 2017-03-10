26c80ade71d1d6cf7334d3ee7964a67fcfd69530
Josefa_shenzhen <289085587@qq.com>
2017-03-10 23:51:46 +0800

    almost!

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -112,15 +112,98 @@
     <section class="main">
         {{--模块一--}}
         <section class="main-one">
-            <div class="main-one-left"></div>
-            <div class="main-one-right"></div>
+            <div class="main-one-left">
+                <div class="main-one-head topic-head">
+                    <span>综艺</span>
+                    <span>更多</span>
+                </div>
+                <div class="main-one-body">
+                    <div class="main-one-block">
+                        <div class="main-one-block-img"><img src="{{ URL::asset('images/210px.png') }}" alt=""></div>
+                        <div class="main-one-block-des">【伦敦之心字幕组】161125 london hearts 男艺人自我排名</div>
+                    </div>
+                    <div class="main-one-block">
+                        <div class="main-one-block-img"><img src="{{ URL::asset('images/210px.png') }}" alt=""></div>
+                        <div class="main-one-block-des">【伦敦之心字幕组】161125 london hearts 男艺人自我排名</div>
+                    </div>
+                    <div class="main-one-block">
+                        <div class="main-one-block-img"><img src="{{ URL::asset('images/210px.png') }}" alt=""></div>
+                        <div class="main-one-block-des">【伦敦之心字幕组】161125 london hearts 男艺人自我排名</div>
+                    </div>
+                    <div class="main-one-block">
+                        <div class="main-one-block-img"><img src="{{ URL::asset('images/210px.png') }}" alt=""></div>
+                        <div class="main-one-block-des">【伦敦之心字幕组】161125 london hearts 男艺人自我排名</div>
+                    </div>
+                    <div class="main-one-block">
+                        <div class="main-one-block-img"><img src="{{ URL::asset('images/210px.png') }}" alt=""></div>
+                        <div class="main-one-block-des">【伦敦之心字幕组】161125 london hearts 男艺人自我排名</div>
+                    </div>
+                    <div class="main-one-block">
+                        <div class="main-one-block-img"><img src="{{ URL::asset('images/210px.png') }}" alt=""></div>
+                        <div class="main-one-block-des">【伦敦之心字幕组】161125 london hearts 男艺人自我排名</div>
+                    </div>
+                </div>
+            </div>
+            <div class="main-one-right">
+                <div class="main-one-head topic-head">
+                    <span>字幕组推荐</span>
+                    <span><  ></span>
+                </div>
+                <div class="main-one-right-body">
+                    <div class="main-one-right-block"></div>
+                    <div class="main-one-right-block"></div>
+                    <div class="main-one-right-block"></div>
+                    <div class="main-one-right-block"></div>
+                </div>
+            </div>
         </section>
         {{--模块一--}}
 
         {{--模块二--}}
         <section class="main-two">
-            <div class="main-tow-left"></div>
-            <div class="main-tow-right"></div>
+            <div class="main-two-left">
+                <div class="main-two-left-head topic-head">
+                    <span>
+                        <ul>
+                            <li>漫才</li>
+                            <li>短剧</li>
+                            <li>段子</li>
+                            <li>番组</li>
+                            </ul>
+                    </span>
+                    <span>更多</span>
+                </div>
+                <div class="main-two-left-body">
+                    <div class="main-two-left-block">
+                        <div class="main-two-left-block-img">
+                            <img src="{{ URL::asset('images/233px.png') }}" alt="">
+                        </div>
+                        <div class="main-two-left-block-des">
+                            <h4>标题</h4>
+                            <p>推荐语推荐语推荐语推荐语推荐语推荐语推荐语推荐语推荐语推荐语推荐语推荐语</p>
+                        </div>
+                    </div>
+                    <div class="main-two-left-block">
+                        <div class="main-two-left-block-img">
+                            <img src="{{ URL::asset('images/233px.png') }}" alt="">
+                        </div>
+                        <div class="main-two-left-block-des">
+                            <h4>标题</h4>
+                            <p>推荐语推荐语推荐语推荐语推荐语推荐语推荐语推荐语推荐语推荐语推荐语推荐语</p>
+                        </div>
+                    </div>
+                    <div class="main-two-left-block">
+                        <div class="main-two-left-block-img">
+                            <img src="{{ URL::asset('images/233px.png') }}" alt="">
+                        </div>
+                        <div class="main-two-left-block-des">
+                            <h4>标题</h4>
+                            <p>推荐语推荐语推荐语推荐语推荐语推荐语推荐语推荐语推荐语推荐语推荐语推荐语</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="main-two-right"></div>
         </section>
         {{--模块二--}}
     </section>
@@ -132,16 +215,16 @@
             <div class="copyRight">
                 <ul>
                     <li>Copyright(C)OWARAICLUB ALL Rights Reserved</li>
-                    <li>转载规则 |</li>
-                    <li>加入我们 |</li>
-                    <li>FAQ |</li>
+                    <li>转载规则&nbsp;&nbsp;|</li>
+                    <li>加入我们&nbsp;&nbsp;|</li>
+                    <li>FAQ&nbsp;&nbsp;|</li>
                     <li>关于我们</li>
                 </ul>
             </div>
             <div class="bottom-logo">
                 <ul>
-                    <li>预留</li>
-                    <li></li>
+                    <li><img src="{{ URL::asset('images/logo_g.png') }}" alt=""></li>
+                    <li><img src="{{ URL::asset('images/black.png') }}" alt=""></li>
                 </ul>
             </div>
         </section>
