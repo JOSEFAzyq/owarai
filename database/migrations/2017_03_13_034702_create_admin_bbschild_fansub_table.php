6bfc668dbcfde61c486ff5b869a07896de8ae67f
Josefa <289085587@qq.com>
2017-03-13 11:52:49 +0800

    migrate实验

---
+++ database/migrations/2017_03_13_034702_create_admin_bbschild_fansub_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminBbschildFansubTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+		Schema::create('admin',function(Blueprint $table){
+			$table->increments('id');
+			$table->string('user_name',20);
+			$table->string('password',32);
+			$table->Integer('bbs_child_id',false,5);
+			$table->Integer('fansub_id',false,5);
+			$table->string('sensitive_auth');
+			$table->string('character',30);
+			$table->dateTime('create_time');
+			$table->dateTime('update_time');
+		});
+
+		Schema::create('fansub',function(Blueprint $table){
+			$table->increments('id');
+			$table->string('name',50);
+			$table->dateTime('create_time');
+			$table->dateTime('update_time');
+		});
+
+		Schema::create('bbs_child',function(Blueprint $table){
+			$table->increments('id');
+			$table->string('name',50);
+			$table->dateTime('create_time');
+			$table->dateTime('update_time');
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
