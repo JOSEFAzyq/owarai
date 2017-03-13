0468e45a9c810214614d6ade985bad56dbfedaae
Josefa <289085587@qq.com>
2017-03-13 11:43:02 +0800

    ~

--- database/migrations/2017_03_13_025615_create_admin_table.php
+++ database/migrations/2017_03_13_025615_create_admin_table.php
@@ -15,15 +15,27 @@ class CreateAdminTable extends Migration
     {
         //
 		Schema::create('admin',function(Blueprint $table){
-			$table->increments('id')->unsigned();
+			$table->increments('id');
 			$table->string('title',50);
 			$table->text('content');
 			$table->string('tags','200');
-			$table->integer('user_id',10)->unsigned();
-			$table->integer('bbschild_id',5)->unsigned();
-			$table->integer('fansub_id',5)->unsigned();
-			$table->integer('authority',3)->unsigned();
-			$table->
+			$table->integer('user_id',false,10);
+			$table->integer('bbschild_id',false,5)->unsigned();
+			$table->integer('fansub_id',false,5)->unsigned();
+			$table->tinyInteger('authority',false,3)->unsigned();
+			$table->integer('replay',false,10)->unsigned();
+			$table->integer('thumb_up',false,10)->unsigned();
+			$table->integer('thumb_down',false,10)->unsigned();
+			$table->integer('view',false,10)->unsigned();
+			$table->integer('pay_amount',false,10)->unsigned();
+			$table->tinyInteger('is_top',false,2)->unsigned();
+			$table->tinyInteger('is_elite',false,3)->unsigned();
+			$table->tinyInteger('is_ori',false,2)->unsigned();
+			$table->tinyInteger('is_pay',false,2)->unsigned();
+			$table->tinyInteger('is_show',false,2)->unsigned();
+			$table->tinyInteger('is_close',false,2)->unsigned();
+			$table->dateTime('create_time');
+			$table->dateTime('update_time');
 
 		});
     }
